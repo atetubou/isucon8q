42972c5be99931ae03894e67ae91d531a4d9e115
autotaker <autotaker@gmail.com>
2018-09-19 17:42:00 +0900

    typo

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -1117,7 +1117,7 @@ func getAdminReportsHandler(c echo.Context) error {
 
 	c.Response().Header().Set("Content-Type", `text/csv; charset=UTF-8`)
 	c.Response().Header().Set("Content-Disposition", `attachment; filename="report.csv"`)
-	_, err := fmt.Fprintf(c.Response(), "reservation_id,event_id,rank,num,price,user_id,sold_at,canceled_at\n")
+	_, err = fmt.Fprintf(c.Response(), "reservation_id,event_id,rank,num,price,user_id,sold_at,canceled_at\n")
 	if err != nil {
 		log.Print("render report csv:", err)
 		return err
