f36db6362481f8fb286202d51760e3d55b5d1b6a
autotaker <autotaker@gmail.com>
2018-09-19 12:59:55 +0900

    alter table

--- db/init.sh
+++ db/init.sh
@@ -18,4 +18,5 @@ fi
 mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon -h$DB_HOST torb
 mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
-mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations ADD KEY user_id_and_time_idx (user_id, IFNULL(canceled_at, reserved_at))'
+mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations add column updated_at DATETIME(6) as (IFNULL(canceled_at, reserved_at)) PERSISTENT'
+mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations ADD KEY user_id_and_time_idx (user_id, updated_at)'
