4178ad464997cca581f19c2018c9eaacd275e5ea
autotaker <autotaker@gmail.com>
2018-09-20 17:37:26 +0900

    typo

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -760,8 +760,8 @@ func postReserveHandler(c echo.Context) error {
 	//adminLock.RLock()
 	//defer adminLock.RUnlock()
 	retryCount := 10
-	retry := 0;
-	for retry < retryCount; retry++ {
+	retry := 0
+	for ; retry < retryCount; retry++ {
 		if err := db.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
 			if err == sql.ErrNoRows {
 				return resError(c, "sold_out", 409)
